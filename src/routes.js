9a8c38a3f251fa9c135bfe74735da2a47449045e
Juliano Meinen <julianomeinen.souza@gmail.com>
2020-02-21 14:00:13 -0300

    Update routes to test

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,14 @@ routes.get("/", function(req, res) {
   return res.send("first route");
 });
 
+routes.get("/test", function(req, res) {
+  return res.send("this is a test");
+});
+
+routes.get("/v1/test", function(req, res) {
+  return res.send("this is a v1 test");
+});
+
 const SequenceController = require("./app/controller/SequenceController");
 
 routes.get("/current", SequenceController.current);
